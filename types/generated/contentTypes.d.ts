cae944720235d047173a6bf1199d2b510d93dc7e
Ankur Pata <ankur.patankar@timedoctor.com>
2024-12-11 19:38:08 +0400

    chore: add image url to schema for articles

--- types/generated/contentTypes.d.ts
+++ types/generated/contentTypes.d.ts
@@ -390,6 +390,7 @@ export interface ApiArticleArticle extends Struct.CollectionTypeSchema {
     createdAt: Schema.Attribute.DateTime;
     createdBy: Schema.Attribute.Relation<'oneToOne', 'admin::user'> &
       Schema.Attribute.Private;
+    image: Schema.Attribute.String;
     locale: Schema.Attribute.String & Schema.Attribute.Private;
     localizations: Schema.Attribute.Relation<
       'oneToMany',
